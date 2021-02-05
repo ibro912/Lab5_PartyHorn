37454aba3ba337dcbf8c9d4e3194213b4119534a
ibro912 <ibro@ucsd.edu>
2021-02-05 04:56:47 -0800

    volume icon feature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -15,6 +15,7 @@ function changeVolume()
     let slider = document.getElementById('volume-slider');
     document.getElementById("horn-sound").volume = sound.value/100.0;
     slider.value = sound.value;
+    changeVolIcon();
 
 }
 
@@ -24,6 +25,35 @@ function changeSlider()
     let sound = document.getElementById('volume-number');
     sound.value = slider.value;
     document.getElementById("horn-sound").volume = sound.value/100.0;
+    changeVolIcon();
+}
+
+function changeVolIcon()
+{
+    let volume = document.getElementById('volume-number');
+    let button = document.getElementById('honk-btn');
+    let volIcon = document.getElementById('volume-image');
+
+    if(volume <= 0)
+    {
+        button.disabled = true;
+        volIcon.src = "./assets/media/icons/volume-level-0.svg";
+    }
+    else if(volume > 0 && volume < 34)
+    {
+        button.disabled = false;
+        volIcon.src = "./assets/media/icons/volume-level-1.svg";
+    }
+    else if(volume > 33 && volume < 67)
+    {
+        button.disabled = false;
+        volIcon.src = "./assets/media/icons/volume-level-2.svg";
+    }
+    else
+    {
+        button.disabled = false;
+        volIcon.src = "./assets/media/icons/volume-level-3.svg";
+    }
 }
 
 function playSound()
@@ -32,6 +62,7 @@ function playSound()
     let horn = document.getElementById("horn-sound");
     horn.play();
 }
+
 
 
 // document.getElementById('volume-number').input = changeVolume(document.getElementById('volume-number').value);
